455534ed1e92019aea42371598f80bbe3b96c79d
Вячеслав Диконов <sdiconov@mail.ru>
2018-05-19 00:09:54 +0300

    Preliminary Russian Translation

---
+++ translations/harbour-amazfish-ru.ts
@@ -0,0 +1,197 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>BipDevice</name>
+    <message>
+        <source>Amazfish</source>
+        <translation>Amazfish</translation>
+    </message>
+    <message>
+        <source>Connected</source>
+        <translation>Подключено</translation>
+    </message>
+    <message>
+        <source>Phone and watch are connected</source>
+        <translation>Телефон и часы связаны</translation>
+    </message>
+</context>
+<context>
+    <name>DebugInfo</name>
+    <message>
+        <source>AmazFish</source>
+        <translation>AmazFish</translation>
+    </message>
+    <message>
+        <source>Refresh</source>
+        <translation>Обновить</translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>Pair with watch</source>
+        <translation>Связать с часами</translation>
+    </message>
+    <message>
+        <source>AmazFish</source>
+        <translation>AmazFish</translation>
+    </message>
+    <message>
+        <source>Connect to watch</source>
+        <translation>Подключиться к часам</translation>
+    </message>
+    <message>
+        <source>Disconnect from watch</source>
+        <translation>Отключиться от часов</translation>
+    </message>
+    <message>
+        <source>Debug Info</source>
+        <translation>Отладка</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Настройка</translation>
+    </message>
+</context>
+<context>
+    <name>PairPage</name>
+    <message>
+        <source>Start scan</source>
+        <translation>Начать поиск</translation>
+    </message>
+</context>
+<context>
+    <name>Settings-device</name>
+    <message>
+        <source>Device Settings</source>
+        <translation>Настройка часов</translation>
+    </message>
+    <message>
+        <source>Langage</source>
+        <translation>Язык</translation>
+    </message>
+    <message>
+        <source>en_US</source>
+        <translation>en_US</translation>
+    </message>
+    <message>
+        <source>es_ES</source>
+        <translation>es_ES</translation>
+    </message>
+    <message>
+        <source>zh_CN</source>
+        <translation>zh_CN</translation>
+    </message>
+    <message>
+        <source>zh_TW</source>
+        <translation>zh_TW</translation>
+    </message>
+    <message>
+        <source>Date Display</source>
+        <translation>Отображение даты</translation>
+    </message>
+    <message>
+        <source>Time</source>
+        <translation>Время</translation>
+    </message>
+    <message>
+        <source>Date/Time</source>
+        <translation>Дата/Время</translation>
+    </message>
+    <message>
+        <source>Time Format</source>
+        <translation>Формат времени</translation>
+    </message>
+    <message>
+        <source>12hr</source>
+        <translation>12ч</translation>
+    </message>
+    <message>
+        <source>24hr</source>
+        <translation>24ч</translation>
+    </message>
+    <message>
+        <source>Save Settings</source>
+        <translation>Сохранить настройки</translation>
+    </message>
+    <message>
+        <source>Distance Unit</source>
+        <translation>Единицы</translation>
+    </message>
+    <message>
+        <source>Metric</source>
+        <translation>Метрические</translation>
+    </message>
+    <message>
+        <source>Imperial</source>
+        <translation>Имперские</translation>
+    </message>
+</context>
+<context>
+    <name>Settings-menu</name>
+    <message>
+        <source>Settings Menu</source>
+        <translation>Меню настройки</translation>
+    </message>
+    <message>
+        <source>User Setting</source>
+        <translation>Пользователь</translation>
+    </message>
+    <message>
+        <source>Device Setting</source>
+        <translation>Устройство</translation>
+    </message>
+</context>
+<context>
+    <name>Settings-profile</name>
+    <message>
+        <source>Profile Settings</source>
+        <translation>Профиль</translation>
+    </message>
+    <message>
+        <source>Name:</source>
+        <translation>Имя:</translation>
+    </message>
+    <message>
+        <source>Gender</source>
+        <translation>Пол</translation>
+    </message>
+    <message>
+        <source>Height (cm): </source>
+        <translation>Рост (см): </translation>
+    </message>
+    <message>
+        <source>Weight (kg): </source>
+        <translation>Вес (кг): </translation>
+    </message>
+    <message>
+        <source>Save Profile</source>
+        <translation>Сохранить профиль</translation>
+    </message>
+    <message>
+        <source>Male</source>
+        <translation>Мужчина</translation>
+    </message>
+    <message>
+        <source>Female</source>
+        <translation>Женщина</translation>
+    </message>
+    <message>
+        <source>Wear Location</source>
+        <translation>Где носятся часы</translation>
+    </message>
+    <message>
+        <source>Left</source>
+        <translation>Левая рука</translation>
+    </message>
+    <message>
+        <source>Right</source>
+        <translation>Правая рука</translation>
+    </message>
+    <message>
+        <source>Goal (steps): </source>
+        <translation>Цель (шагов):  </translation>
+    </message>
+</context>
+</TS>
