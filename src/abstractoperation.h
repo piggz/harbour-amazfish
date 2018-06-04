be4dacc4baf87d278474d7432009316d997906de
Adam Pigg <adam@piggz.co.uk>
2018-06-04 16:22:26 +0100

    fix warnings

--- src/abstractoperation.h
+++ src/abstractoperation.h
@@ -7,7 +7,8 @@ class AbstractOperation
 {
 public:
     AbstractOperation(QBLEService *service);
-
+    virtual ~AbstractOperation(){};
+    
     virtual void start() = 0;
     virtual bool handleMetaData(const QByteArray &meta) = 0;
     virtual void handleData(const QByteArray &data) = 0;
