f92679b7f62864586cf97da9bf060fa31c2ccf02
Adam Pigg <adam@piggz.co.uk>
2018-06-18 07:51:06 +0100

    add basic firmware service

---
+++ src/services/bipfirmwareservice.cpp
@@ -0,0 +1,19 @@
+#include "bipfirmwareservice.h"
+
+const char* BipFirmwareService::UUID_SERVICE_BIP_FIRMWARE = "00001530-0000-3512-2118-0009af100700";
+const char* BipFirmwareService::UUID_CHARACTERISTIC_FIRMWARE = "00001531-0000-3512-2118-0009af100700";
+const char* BipFirmwareService::UUID_CHARACTERISTIC_FIRMWARE_DATA = "00001532-0000-3512-2118-0009af100700";
+
+
+BipFirmwareService::BipFirmwareService(const QString &path, QObject *parent) : QBLEService(UUID_SERVICE_BIP_FIRMWARE, path, parent)
+{
+    qDebug() << "BipFirmwareService::BipFirmwareService";
+
+    connect(this, &QBLEService::characteristicChanged, this, &BipFirmwareService::characteristicChanged);
+}
+
+
+void BipFirmwareService::characteristicChanged(const QString &characteristic, const QByteArray &value)
+{
+    qDebug() << "FW Changed:" << characteristic << value;
+}
