88ec29acf90fafe72eb5fde771f4e45c6cef5d6d
dashinfantry <45334556+dashinfantry@users.noreply.github.com>
2019-04-12 21:25:10 +0800

    Create harbour-amazfish-zh_cn.ts

---
+++ translations/harbour-amazfish-zh_cn.ts
@@ -0,0 +1,617 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>AbstractDevice</name>
+    <message>
+        <source>Device doen not support settings</source>
+        <translation>设备不支持设置</translation>
+    </message>
+</context>
+<context>
+    <name>AddCityDialog</name>
+    <message>
+        <source>Set Location</source>
+        <translation>设置位置</translation>
+    </message>
+</context>
+<context>
+    <name>Alarm</name>
+    <message>
+        <source>Enabled</source>
+        <translation>已启用</translation>
+    </message>
+    <message>
+        <source>Mon</source>
+        <translation>周一</translation>
+    </message>
+    <message>
+        <source>Tue</source>
+        <translation>周二</translation>
+    </message>
+    <message>
+        <source>Wed</source>
+        <translation>周三</translation>
+    </message>
+    <message>
+        <source>Thu</source>
+        <translation>周四</translation>
+    </message>
+    <message>
+        <source>Fri</source>
+        <translation>周五</translation>
+    </message>
+    <message>
+        <source>Sat</source>
+        <translation>周六</translation>
+    </message>
+    <message>
+        <source>Sun</source>
+        <translation>周日</translation>
+    </message>
+</context>
+<context>
+    <name>AnalysisPage</name>
+    <message>
+        <source>Download Data</source>
+        <translation>下载数据</translation>
+    </message>
+    <message>
+        <source>Analysis</source>
+        <translation>分析</translation>
+    </message>
+    <message>
+        <source>Heartrate</source>
+        <translation>心率</translation>
+    </message>
+    <message>
+        <source>Steps</source>
+        <translation>步数</translation>
+    </message>
+    <message>
+        <source>Intensity</source>
+        <translation>强度</translation>
+    </message>
+</context>
+<context>
+    <name>BipDevice</name>
+    <message>
+        <source>Amazfish</source>
+        <translation>Amazfish</translation>
+    </message>
+    <message>
+        <source>Connected</source>
+        <translation>已连接</translation>
+    </message>
+    <message>
+        <source>Phone and watch are connected</source>
+        <translation>手机和手表已连接</translation>
+    </message>
+</context>
+<context>
+    <name>BipFirmwarePage</name>
+    <message>
+        <source>Send file</source>
+        <translation>发送文件</translation>
+    </message>
+    <message>
+        <source>Download File</source>
+        <translation>下载文件</translation>
+    </message>
+    <message>
+        <source>Select a file to download.</source>
+        <translation>选择要下载的文件</translation>
+    </message>
+    <message>
+        <source>None</source>
+        <translation>无</translation>
+    </message>
+    <message>
+        <source>Choose File</source>
+        <translation>选择文件</translation>
+    </message>
+    <message>
+        <source>File type/version: </source>
+        <translation>文件类型/版本</translation>
+    </message>
+</context>
+<context>
+    <name>BipFirmwareService</name>
+    <message>
+        <source>An operation is currently running, please try later</source>
+        <translation>正在执行一项操作，请稍后再试</translation>
+    </message>
+    <message>
+        <source>No file selected</source>
+        <translation>没有选取的文件</translation>
+    </message>
+</context>
+<context>
+    <name>DebugInfo</name>
+    <message>
+        <source>Refresh</source>
+        <translation>刷新</translation>
+    </message>
+    <message>
+        <source>AmazFish</source>
+        <translation>AmazFish</translation>
+    </message>
+    <message>
+        <source>Address: </source>
+        <translation>地址：</translation>
+    </message>
+    <message>
+        <source>Serial No: </source>
+        <translation>序列号：</translation>
+    </message>
+    <message>
+        <source>Hardware Rev: </source>
+        <translation>硬件版本：</translation>
+    </message>
+    <message>
+        <source>Software Rev: </source>
+        <translation>软件版本:</translation>
+    </message>
+    <message>
+        <source>Connection State: </source>
+        <translation>连接状态：</translation>
+    </message>
+    <message>
+        <source>GPS Ver: </source>
+        <translation>GPS 视图：</translation>
+    </message>
+    <message>
+        <source>Test Notification</source>
+        <translation>测试通知</translation>
+    </message>
+    <message>
+        <source>Test Email</source>
+        <translation>测试电子邮件</translation>
+    </message>
+    <message>
+        <source>Test Call</source>
+        <translation>测试通话</translation>
+    </message>
+    <message>
+        <source>Fetch debug log</source>
+        <translation>获取调试日志</translation>
+    </message>
+    <message>
+        <source>Test Popup</source>
+        <translation>测试弹出通知</translation>
+    </message>
+    <message>
+        <source>Reboot watch</source>
+        <translation>重启手表</translation>
+    </message>
+</context>
+<context>
+    <name>DeviceInterface</name>
+    <message>
+        <source>Device is not valid, it may not be supported</source>
+        <translation>设备无效，可能不受支持</translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>Debug Info</source>
+        <translation>诊断信息</translation>
+    </message>
+    <message>
+        <source>Pair with watch</source>
+        <translation>和手表配对</translation>
+    </message>
+    <message>
+        <source>Download File</source>
+        <translation>下载文件</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Disconnect from watch</source>
+        <translation>和手表短开连接</translation>
+    </message>
+    <message>
+        <source>Connect to watch</source>
+        <translation>连接到手表</translation>
+    </message>
+    <message>
+        <source>AmazFish</source>
+        <translation>AmazFish</translation>
+    </message>
+</context>
+<context>
+    <name>GraphData</name>
+    <message>
+        <source>No data</source>
+        <translation>无数据</translation>
+    </message>
+</context>
+<context>
+    <name>MiBandService</name>
+    <message>
+        <source>An operation is currently running, please try later</source>
+        <translation>正在执行一项操作，请稍后再试</translation>
+    </message>
+    <message>
+        <source>Firmware is too old to set display items, V0.1.1.14 is required</source>
+        <translation>固件太旧，无法设置显示项目，需要 V0.1.1.14</translation>
+    </message>
+    <message>
+        <source>Firmware supports weather condition string</source>
+        <translation>固件支持显示天气状态</translation>
+    </message>
+</context>
+<context>
+    <name>PairPage</name>
+    <message>
+        <source>Start scan</source>
+        <translation>开始扫描</translation>
+    </message>
+    <message>
+        <source>Try again</source>
+        <translation>再次尝试</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>Finished transferring activity data</source>
+        <translation>传输运动数据已完成</translation>
+    </message>
+    <message>
+        <source>No data to transfer</source>
+        <translation>无传输的数据</translation>
+    </message>
+    <message>
+        <source>Update operation failed</source>
+        <translation>更新操作失败</translation>
+    </message>
+    <message>
+        <source>Update operation failed, unexpected metadata</source>
+        <translation>更新操作失败，意外的元数据</translation>
+    </message>
+    <message>
+        <source>Update operation complete</source>
+        <translation>更新操作完成</translation>
+    </message>
+    <message>
+        <source>File does not seem to be supported</source>
+        <translation>文件似乎不受支持</translation>
+    </message>
+    <message>
+        <source>About to transfer data from </source>
+        <translation>即将传输数据自</translation>
+    </message>
+</context>
+<context>
+    <name>Settings-alarms</name>
+    <message>
+        <source>Alarms</source>
+        <translation>闹钟</translation>
+    </message>
+    <message>
+        <source>Save Settings</source>
+        <translation>保存设置</translation>
+    </message>
+    <message>
+        <source>Alarm 1</source>
+        <translation>闹钟1</translation>
+    </message>
+    <message>
+        <source>Alarm 2</source>
+        <translation>闹钟2</translation>
+    </message>
+    <message>
+        <source>Alarm 3</source>
+        <translation>闹钟3</translation>
+    </message>
+    <message>
+        <source>Alarm 4</source>
+        <translation>闹钟4</translation>
+    </message>
+    <message>
+        <source>Alarm 5</source>
+        <translation>闹钟5</translation>
+    </message>
+</context>
+<context>
+    <name>Settings-app</name>
+    <message>
+        <source>Device Settings</source>
+        <translation>设备设置</translation>
+    </message>
+    <message>
+        <source>Notify on connect</source>
+        <translation>连接通知</translation>
+    </message>
+    <message>
+        <source>Save Settings</source>
+        <translation>保存设置</translation>
+    </message>
+    <message>
+        <source>Refresh weather every (</source>
+        <translation>刷新天气频率</translation>
+    </message>
+    <message>
+        <source>) minutes</source>
+        <translation>分钟</translation>
+    </message>
+    <message>
+        <source>Sync activity data each hour</source>
+        <translation>每小时同步运动数据</translation>
+    </message>
+</context>
+<context>
+    <name>Settings-bip-shortcuts</name>
+    <message>
+        <source>BIP Display Items</source>
+        <translation>BIP显示项目</translation>
+    </message>
+    <message>
+        <source>Status</source>
+        <translation>状态</translation>
+    </message>
+    <message>
+        <source>Activity</source>
+        <translation>活动</translation>
+    </message>
+    <message>
+        <source>Weather</source>
+        <translation>天气</translation>
+    </message>
+    <message>
+        <source>Alarm</source>
+        <translation>闹钟</translation>
+    </message>
+    <message>
+        <source>Timer</source>
+        <translation>定时器</translation>
+    </message>
+    <message>
+        <source>Compass</source>
+        <translation>指南针</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>设置</translation>
+    </message>
+    <message>
+        <source>AliPay</source>
+        <translation>支付宝</translation>
+    </message>
+    <message>
+        <source>Weather Shortcut</source>
+        <translation>天气捷径</translation>
+    </message>
+    <message>
+        <source>AliPay Shortcut</source>
+        <translation>支付宝捷径</translation>
+    </message>
+    <message>
+        <source>Save Settings</source>
+        <translation>保存设置</translation>
+    </message>
+</context>
+<context>
+    <name>Settings-device</name>
+    <message>
+        <source>Device Settings</source>
+        <translation>设备设置</translation>
+    </message>
+    <message>
+        <source>Langage</source>
+        <translation>语言</translation>
+    </message>
+    <message>
+        <source>en_US</source>
+        <translation>美式英语</translation>
+    </message>
+    <message>
+        <source>es_ES</source>
+        <translation>英式英语</translation>
+    </message>
+    <message>
+        <source>zh_CN</source>
+        <translation>简体汉语</translation>
+    </message>
+    <message>
+        <source>zh_TW</source>
+        <translation>繁体汉语</translation>
+    </message>
+    <message>
+        <source>Date Display</source>
+        <translation>日期显示</translation>
+    </message>
+    <message>
+        <source>Time</source>
+        <translation>时间</translation>
+    </message>
+    <message>
+        <source>Date/Time</source>
+        <translation>日期/时间</translation>
+    </message>
+    <message>
+        <source>Time Format</source>
+        <translation>时间格式</translation>
+    </message>
+    <message>
+        <source>12hr</source>
+        <translation>12小时制</translation>
+    </message>
+    <message>
+        <source>24hr</source>
+        <translation>24小时制</translation>
+    </message>
+    <message>
+        <source>Save Settings</source>
+        <translation>保存设置</translation>
+    </message>
+    <message>
+        <source>Distance Unit</source>
+        <translation>距离单位</translation>
+    </message>
+    <message>
+        <source>Metric</source>
+        <translation>公制</translation>
+    </message>
+    <message>
+        <source>Imperial</source>
+        <translation>英制</translation>
+    </message>
+    <message>
+        <source>BIP Display Items</source>
+        <translation>BIP 显示项目</translation>
+    </message>
+    <message>
+        <source>ru_RU</source>
+        <translation>俄罗斯俄语</translation>
+    </message>
+</context>
+<context>
+    <name>Settings-menu</name>
+    <message>
+        <source>Settings Menu</source>
+        <translation>设置菜单</translation>
+    </message>
+    <message>
+        <source>User Settings</source>
+        <translation>用户设置</translation>
+    </message>
+    <message>
+        <source>Device Settings</source>
+        <translation>设备设置</translation>
+    </message>
+    <message>
+        <source>Application Settings</source>
+        <translation>应用程序设置</translation>
+    </message>
+    <message>
+        <source>Alarms</source>
+        <translation>闹钟</translation>
+    </message>
+    <message>
+        <source>Donate</source>
+        <translation>捐赠</translation>
+    </message>
+    <message>
+        <source>Weather</source>
+        <translation>天气</translation>
+    </message>
+</context>
+<context>
+    <name>Settings-profile</name>
+    <message>
+        <source>Profile Settings</source>
+        <translation>配置设置</translation>
+    </message>
+    <message>
+        <source>Name:</source>
+        <translation>名称：</translation>
+    </message>
+    <message>
+        <source>Birthday</source>
+        <translation>生日</translation>
+    </message>
+    <message>
+        <source>Gender</source>
+        <translation>性别</translation>
+    </message>
+    <message>
+        <source>Male</source>
+        <translation>男性</translation>
+    </message>
+    <message>
+        <source>Female</source>
+        <translation>女性</translation>
+    </message>
+    <message>
+        <source>Height (cm): </source>
+        <translation>身高（厘米）</translation>
+    </message>
+    <message>
+        <source>Weight (kg): </source>
+        <translation>体重（千克）</translation>
+    </message>
+    <message>
+        <source>Wear Location</source>
+        <translation>穿戴位置</translation>
+    </message>
+    <message>
+        <source>Left</source>
+        <translation>左手</translation>
+    </message>
+    <message>
+        <source>Right</source>
+        <translation>右手</translation>
+    </message>
+    <message>
+        <source>Display on lift wrist</source>
+        <translation>抬腕亮屏</translation>
+    </message>
+    <message>
+        <source>Goal (steps): </source>
+        <translation>目标（步数）</translation>
+    </message>
+    <message>
+        <source>Alert on fitness goal</source>
+        <translation>健康目标提醒</translation>
+    </message>
+    <message>
+        <source>Use HRM for Sleep Detection</source>
+        <translation>使用心率监测器以进行睡眠监测</translation>
+    </message>
+    <message>
+        <source>All day HRM interval (minutes): </source>
+        <translation>全天心率监测间隔（分钟）：</translation>
+    </message>
+    <message>
+        <source>Save Profile</source>
+        <translation>保存配置</translation>
+    </message>
+</context>
+<context>
+    <name>SleepPage</name>
+    <message>
+        <source>Download Data</source>
+        <translation>下载数据</translation>
+    </message>
+    <message>
+        <source>Sleep Summary</source>
+        <translation>睡眠总结</translation>
+    </message>
+    <message>
+        <source>Sleep</source>
+        <translation>睡眠</translation>
+    </message>
+    <message>
+        <source>hrs</source>
+        <translation>hrs</translation>
+    </message>
+</context>
+<context>
+    <name>SportsSummaryPage</name>
+    <message>
+        <source>Sports Activities</source>
+        <translation>运动活动</translation>
+    </message>
+    <message>
+        <source>Download Next Activity</source>
+        <translation>下载下一个活动</translation>
+    </message>
+</context>
+<context>
+    <name>StepsPage</name>
+    <message>
+        <source>Download Data</source>
+        <translation>下载数据</translation>
+    </message>
+    <message>
+        <source>Steps</source>
+        <translation>步数</translation>
+    </message>
+</context>
+</TS>
