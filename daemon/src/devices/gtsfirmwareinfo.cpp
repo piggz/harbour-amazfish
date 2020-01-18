1d4f9a37b67a867779cb72151c3f5cb1dc91f111
Adam Pigg <adam@piggz.co.uk>
2020-01-18 20:49:57 +0000

    Add GtsFirmwareInfo to handle GTS file downloads

---
+++ daemon/src/devices/gtsfirmwareinfo.cpp
@@ -0,0 +1,6 @@
+#include "gtsfirmwareinfo.h"
+
+GtsFirmwareInfo::GtsFirmwareInfo()
+{
+
+}
---
+++ daemon/src/devices/gtsfirmwareinfo.h
@@ -0,0 +1,11 @@
+#ifndef GTSFIRMWAREINFO_H
+#define GTSFIRMWAREINFO_H
+
+
+class GtsFirmwareInfo : public AbstractFirmwareInfo
+{
+public:
+    GtsFirmwareInfo();
+};
+
+#endif // GTSFIRMWAREINFO_H
