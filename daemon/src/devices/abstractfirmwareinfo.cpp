caea0f85e24fea1ae6f39b0171db8148129422e3
Adam Pigg <adam@piggz.co.uk>
2020-02-27 20:17:30 +0000

    Start an implementation of CRC32

--- daemon/src/devices/abstractfirmwareinfo.cpp
+++ daemon/src/devices/abstractfirmwareinfo.cpp
@@ -37,3 +37,26 @@ uint16_t AbstractFirmwareInfo::crc16() const
 {
     return m_crc16;
 }
+
+void AbstractFirmwareInfo::calculateCRC32() {
+    uint16_t crc = 0xFFFF;
+
+    if (m_bytes.length() > 0) {
+        int len = m_bytes.length();
+        for (int i =0; i < len; ++i) {
+            char byte = m_bytes[i];
+            crc = (crc >> 8) | (crc << 8);
+            crc ^= byte;
+            crc ^= ((unsigned char) crc) >> 4;
+            crc ^= crc << 12;
+            crc ^= (crc & 0xFF) << 5;
+        }
+    }
+
+    m_crc16 = crc;
+}
+
+uint32_t AbstractFirmwareInfo::crc32() const
+{
+    return m_crc16;
+}
