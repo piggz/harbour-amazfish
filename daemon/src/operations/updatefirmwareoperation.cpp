9daee185b43931a7f3b6f35c7c07962f6866d7a3
Adam Pigg <adam@piggz.co.uk>
2020-02-29 11:03:25 +0000

    [daemon] Experiment with firmware download for GTS

--- daemon/src/operations/updatefirmwareoperation.cpp
+++ daemon/src/operations/updatefirmwareoperation.cpp
@@ -83,20 +83,25 @@ bool UpdateFirmwareOperation::sendFwInfo()
 {
     int fwSize = m_fwBytes.size();
     QByteArray sizeBytes = TypeConversion::fromInt24(fwSize);
-    int arraySize = 4;
-    bool isFirmwareCode = m_info->type() == AbstractFirmwareInfo::Firmware;
-    if (!isFirmwareCode) {
-        arraySize++;
-    }
+    int arraySize = 10;
+    //bool isFirmwareCode = m_info->type() == AbstractFirmwareInfo::Firmware;
+    //if (!isFirmwareCode) {
+    //    arraySize++;
+    //}
     QByteArray bytes(arraySize, 0x00);
     int i = 0;
     bytes[i++] = BipFirmwareService::COMMAND_FIRMWARE_INIT;
+    bytes[i++] = m_info->type();
     bytes[i++] = sizeBytes[0];
     bytes[i++] = sizeBytes[1];
     bytes[i++] = sizeBytes[2];
-    if (!isFirmwareCode) {
-        bytes[i++] = m_info->type();
-    }
+    bytes[i++] = 0; // TODO: what is that?
+    //int crc32 = firmwareInfo.getCrc32();
+    //byte[] crcBytes = BLETypeConversions.fromUint32(crc32);
+    //bytes[i++] = crcBytes[0];
+    //bytes[i++] = crcBytes[1];
+    //bytes[i++] = crcBytes[2];
+    //bytes[i] = crcBytes[3];
 
     m_service->writeValue(BipFirmwareService::UUID_CHARACTERISTIC_FIRMWARE, bytes);
     return true;
