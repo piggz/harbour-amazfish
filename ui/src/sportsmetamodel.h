ce9e9f5834e1add840b2af949b7758dbaf095502
Adam Pigg <adam@piggz.co.uk>
2020-10-08 19:32:33 +0100

    Add basics for new sports data page

---
+++ ui/src/sportsmetamodel.h
@@ -0,0 +1,45 @@
+#ifndef SPORTSDATAMODEL_H
+#define SPORTSDATAMODEL_H
+
+#include <QAbstractListModel>
+#include <QVariant>
+
+#include <KDb3/KDbConnection>
+
+typedef struct {
+    int id;
+    int sport_id;
+    QString key;
+    QString value;
+    QString unit;
+} SportsMeta;
+
+class SportsMetaModel : public QAbstractListModel
+{
+    Q_OBJECT
+public:
+    SportsMetaModel();
+
+    enum Roles {
+        SportMetaId = Qt::UserRole + 1,
+        SportId,
+        SportKey,
+        SportValue,
+        SportUnit
+    };
+
+
+    virtual QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+    virtual QHash<int, QByteArray> 	roleNames() const override;
+    virtual int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+
+    void setConnection(KDbConnection *conn);
+
+    Q_INVOKABLE void update();
+
+private:
+    KDbConnection *m_connection;
+    QList<SportsMeta> m_data;
+};
+
+#endif // SPORTSDATAMODEL_H
