178ba9e4c15d49300855cc4534f7cbe385f781d2
Adam Pigg <adam@piggz.co.uk>
2021-01-19 22:24:29 +0000

    Update to the new music service UUIDs

--- daemon/src/services/pinetimemusicservice.cpp
+++ daemon/src/services/pinetimemusicservice.cpp
@@ -1,11 +1,11 @@
 #include "pinetimemusicservice.h"
 
-const char* PineTimeMusicService::UUID_SERVICE_MUSIC = "c7e50001-00fc-48fe-8e23-433b3a1942d0";
-const char* PineTimeMusicService::UUID_CHARACTERISTIC_MUSIC_EVENT = "c7e50002-00fc-48fe-8e23-433b3a1942d0";
-const char* PineTimeMusicService::UUID_CHARACTERISTIC_MUSIC_STATUS = "c7e50003-00fc-48fe-8e23-433b3a1942d0";
-const char* PineTimeMusicService::UUID_CHARACTERISTIC_MUSIC_ARTIST = "c7e50004-00fc-48fe-8e23-433b3a1942d0";
-const char* PineTimeMusicService::UUID_CHARACTERISTIC_MUSIC_TRACK = "c7e50005-00fc-48fe-8e23-433b3a1942d0";
-const char* PineTimeMusicService::UUID_CHARACTERISTIC_MUSIC_ALBUM = "c7e50006-00fc-48fe-8e23-433b3a1942d0";
+const char* PineTimeMusicService::UUID_SERVICE_MUSIC = "00000000-78fc-48fe-8e23-433b3a1942d0";
+const char* PineTimeMusicService::UUID_CHARACTERISTIC_MUSIC_EVENT = "00000001-78fc-48fe-8e23-433b3a1942d0";
+const char* PineTimeMusicService::UUID_CHARACTERISTIC_MUSIC_STATUS = "00000002-78fc-48fe-8e23-433b3a1942d0";
+const char* PineTimeMusicService::UUID_CHARACTERISTIC_MUSIC_ARTIST = "00000003-78fc-48fe-8e23-433b3a1942d0";
+const char* PineTimeMusicService::UUID_CHARACTERISTIC_MUSIC_TRACK = "00000004-78fc-48fe-8e23-433b3a1942d0";
+const char* PineTimeMusicService::UUID_CHARACTERISTIC_MUSIC_ALBUM = "00000005-78fc-48fe-8e23-433b3a1942d0";
 
 PineTimeMusicService::PineTimeMusicService(const QString &path, QObject *parent) : QBLEService(UUID_SERVICE_MUSIC, path, parent)
 {
