e610fe55856bb739de1e6a871f5503765b0f01e2
Adam Pigg <adam@piggz.co.uk>
2022-04-03 21:11:09 +0100

    Override initialise for gtr2device to remove size property

--- daemon/src/devices/gtr2device.h
+++ daemon/src/devices/gtr2device.h
@@ -8,6 +8,9 @@ class Gtr2Device : public GtrDevice
 {
 public:
     explicit Gtr2Device(const QString &pairedName, QObject *parent = nullptr);
+
+protected:
+    void initialise() override;
 };
 
 #endif // GTR2DEVICE_H
