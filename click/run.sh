55c2f17426469fdae72a782b3f18e126daa8a4ba
Jozef Mlich <jmlich83@gmail.com>
2023-08-07 11:29:06 +0200

    clickable: install bundled libraries

--- click/run.sh
+++ click/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export LD_LIBRARY_PATH=$PWD/usr/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$PWD/lib:$LD_LIBRARY_PATH
 export QT_QUICK_CONTROLS_MOBILE=true
 export QT_QUICK_CONTROLS_STYLE=Suru
 
