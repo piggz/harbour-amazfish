101d57298c31dc34c3f4ca60489a0066d873dc52
Jozef Mlich <jmlich83@gmail.com>
2023-08-07 09:27:32 +0200

    click packaging similar to harbour-owncloud

---
+++ click/run-daemon.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+export LD_LIBRARY_PATH=$PWD/usr/lib:$LD_LIBRARY_PATH
+export QT_QUICK_CONTROLS_MOBILE=true
+export QT_QUICK_CONTROLS_STYLE=Material
+
+exec $PWD/bin/harbour-amazfishd \
+    --desktop_file_hint=${HOME}/.local/share/applications/${APP_ID}.desktop "$@"
