30bd04012192f8eadb1a8ed3e6c286294ad90c8c
Jozef Mlich <jmlich83@gmail.com>
2023-09-18 08:36:12 +0200

    fix systemd wants

--- click/run.sh
+++ click/run.sh
@@ -18,6 +18,9 @@ RestartSec=5
 Environment=LD_LIBRARY_PATH=/opt/click.ubuntu.com/harbour-amazfish/current/lib:/opt/click.ubuntu.com/harbour-amazfish/current/lib/aarch64-linux-gnu:$LD_LIBRARY_PATH
 Environment=HOME=%h XDG_CONFIG_HOME=/home/%u/.config DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/%U/bus XDG_RUNTIME_DIR=/run/user/%U
 
+[Install]
+WantedBy=default.target
+
 EOF
 
     /usr/bin/systemctl daemon-reload
@@ -32,6 +35,12 @@ else
     enter_active=$(date -d "$(systemctl show -p ActiveEnterTimestamp --value --user harbour-amazfish)" +%s) #"
     if [ "$mod_time" -gt "$enter_active" ]; then
         /usr/bin/systemctl --user restart harbour-amazfish.service
+    fi
+
+    # start if not running
+
+    if ! systemctl is-active --user harbour-amazfish; then
+        /usr/bin/systemctl --user start harbour-amazfish.service
     fi
 
 fi
