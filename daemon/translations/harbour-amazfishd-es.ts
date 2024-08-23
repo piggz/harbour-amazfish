a061a252a0850f585123721968457e5e02948944
gallegonovato <fran-carro@hotmail.es>
2024-08-23 08:01:44 +0000

    Translated using Weblate (Spanish)
    Currently translated at 100.0% (29 of 29 strings)
    
    Translation: Amazfish/Daemon
    Translate-URL: https://hosted.weblate.org/projects/harbour-amazfish/daemon/es/

--- daemon/translations/harbour-amazfishd-es.ts
+++ daemon/translations/harbour-amazfishd-es.ts
@@ -5,143 +5,143 @@
     <name>AbstractDevice</name>
     <message>
         <source>Device does not support settings</source>
-        <translation type="unfinished"></translation>
+        <translation>El dispositivo no admite ajustes</translation>
     </message>
 </context>
 <context>
     <name>AsteroidOSDevice</name>
     <message>
         <source>Stored %1...</source>
-        <translation type="unfinished"></translation>
+        <translation>Almacenado %1...</translation>
     </message>
 </context>
 <context>
     <name>BipDevice</name>
     <message>
         <source>Navigation Started</source>
-        <translation type="unfinished"></translation>
+        <translation>Navegación iniciada</translation>
     </message>
     <message>
         <source>Navigation Stopped</source>
-        <translation type="unfinished"></translation>
+        <translation>Navegación detenida</translation>
     </message>
     <message>
         <source>Progress</source>
-        <translation type="unfinished"></translation>
+        <translation>Progreso</translation>
     </message>
 </context>
 <context>
     <name>BipFirmwareService</name>
     <message>
         <source>An operation is currently running, please try later</source>
-        <translation type="unfinished"></translation>
+        <translation>Se está ejecutando una operación, por favor inténtalo más tarde</translation>
     </message>
     <message>
         <source>Sending %1...</source>
-        <translation type="unfinished"></translation>
+        <translation>Enviando %1...</translation>
     </message>
     <message>
         <source>No file selected</source>
-        <translation type="unfinished"></translation>
+        <translation>Ningún archivo seleccionado</translation>
     </message>
 </context>
 <context>
     <name>DeviceInterface</name>
     <message>
         <source>Device is not valid, it may not be supported</source>
-        <translation type="unfinished"></translation>
+        <translation>El dispositivo no es válido, puede que no sea compatible</translation>
     </message>
     <message>
         <source>Amazfish</source>
-        <translation type="unfinished"></translation>
+        <translation>Amazfish</translation>
     </message>
     <message>
         <source>Connected</source>
-        <translation type="unfinished"></translation>
+        <translation>Conectado</translation>
     </message>
     <message>
         <source>Phone and watch are connected</source>
-        <translation type="unfinished"></translation>
+        <translation>Teléfono y reloj conectados</translation>
     </message>
     <message>
         <source>Low Battery</source>
-        <translation type="unfinished"></translation>
+        <translation>Batería baja</translation>
     </message>
     <message>
         <source>Battery level now </source>
-        <translation type="unfinished"></translation>
+        <translation>Nivel de batería actual </translation>
     </message>
 </context>
 <context>
     <name>DfuService</name>
     <message>
         <source>An operation is currently running, please try later</source>
-        <translation type="unfinished"></translation>
+        <translation>Se está ejecutando una operación, inténtalo más tarde</translation>
     </message>
     <message>
         <source>No file selected</source>
-        <translation type="unfinished"></translation>
+        <translation>Ningún archivo seleccionado</translation>
     </message>
 </context>
 <context>
     <name>HuamiDevice</name>
     <message>
         <source>Navigation Started</source>
-        <translation type="unfinished"></translation>
+        <translation>Navegación iniciada</translation>
     </message>
     <message>
         <source>Navigation Stopped</source>
-        <translation type="unfinished"></translation>
+        <translation>Navegación detenida</translation>
     </message>
     <message>
         <source>Progress</source>
-        <translation type="unfinished"></translation>
+        <translation>Progreso</translation>
     </message>
 </context>
 <context>
     <name>MiBandService</name>
     <message>
         <source>Firmware is too old to set display items, V0.1.1.14 is required</source>
-        <translation type="unfinished"></translation>
+        <translation>El firmware es demasiado antiguo para configurar los elementos en la pantalla, se requiere V0.1.1.14</translation>
     </message>
     <message>
         <source>An operation is currently running, please try later</source>
-        <translation type="unfinished"></translation>
+        <translation>Se está ejecutando una operación, por favor inténtalo más tarde</translation>
     </message>
 </context>
 <context>
     <name>QObject</name>
     <message>
         <source>About to transfer data from </source>
-        <translation type="unfinished"></translation>
+        <translation>A punto de transferir datos desde </translation>
     </message>
     <message>
         <source>Finished transferring activity data</source>
-        <translation type="unfinished"></translation>
+        <translation>Finalizada la transferencia de datos</translation>
     </message>
     <message>
         <source>No data to transfer</source>
-        <translation type="unfinished"></translation>
+        <translation>Sin datos para transferir</translation>
     </message>
     <message>
         <source>Update operation failed</source>
-        <translation type="unfinished"></translation>
+        <translation>Error en la actualización</translation>
     </message>
     <message>
         <source>Update operation failed, unexpected metadata</source>
-        <translation type="unfinished"></translation>
+        <translation>Error en la actualización, metadatos inesperados</translation>
     </message>
     <message>
         <source>Update operation complete</source>
-        <translation type="unfinished"></translation>
+        <translation>Actualización completada</translation>
     </message>
     <message>
         <source>Update operation failed, filetype not supported</source>
-        <translation type="unfinished"></translation>
+        <translation>Error en la actualización, no se admite este tipo de archivo</translation>
     </message>
     <message>
         <source>File does not seem to be supported</source>
-        <translation type="unfinished"></translation>
+        <translation>El archivo parece ser incompatible</translation>
     </message>
 </context>
 </TS>
