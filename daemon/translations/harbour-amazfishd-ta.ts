9d671488ba4579d5818ef70a6e67a50473a2740b
தமிழ்நேரம் <anishprabu.t@gmail.com>
2024-11-30 14:01:53 +0000

    Translated using Weblate (Tamil)
    Currently translated at 100.0% (29 of 29 strings)
    
    Translation: Amazfish/Daemon
    Translate-URL: https://hosted.weblate.org/projects/harbour-amazfish/daemon/ta/

--- daemon/translations/harbour-amazfishd-ta.ts
+++ daemon/translations/harbour-amazfishd-ta.ts
@@ -5,143 +5,143 @@
     <name>AbstractDevice</name>
     <message>
         <source>Device does not support settings</source>
-        <translation type="unfinished"></translation>
+        <translation>சாதனம் அமைப்புகளை ஆதரிக்காது</translation>
     </message>
 </context>
 <context>
     <name>AsteroidOSDevice</name>
     <message>
         <source>Stored %1...</source>
-        <translation type="unfinished"></translation>
+        <translation>சேமிக்கப்பட்டது %1 ...</translation>
     </message>
 </context>
 <context>
     <name>BipDevice</name>
     <message>
         <source>Navigation Started</source>
-        <translation type="unfinished"></translation>
+        <translation>வழிசெலுத்தல் தொடங்கியது</translation>
     </message>
     <message>
         <source>Navigation Stopped</source>
-        <translation type="unfinished"></translation>
+        <translation>வழிசெலுத்தல் நிறுத்தப்பட்டது</translation>
     </message>
     <message>
         <source>Progress</source>
-        <translation type="unfinished"></translation>
+        <translation>முன்னேற்றம்</translation>
     </message>
 </context>
 <context>
     <name>BipFirmwareService</name>
     <message>
         <source>An operation is currently running, please try later</source>
-        <translation type="unfinished"></translation>
+        <translation>ஒரு செயல்பாடு தற்போது இயங்குகிறது, தயவுசெய்து பின்னர் முயற்சிக்கவும்</translation>
     </message>
     <message>
         <source>Sending %1...</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 ஐ அனுப்புகிறது ...</translation>
     </message>
     <message>
         <source>No file selected</source>
-        <translation type="unfinished"></translation>
+        <translation>எந்த கோப்பும் தேர்ந்தெடுக்கப்படவில்லை</translation>
     </message>
 </context>
 <context>
     <name>DeviceInterface</name>
     <message>
         <source>Device is not valid, it may not be supported</source>
-        <translation type="unfinished"></translation>
+        <translation>சாதனம் செல்லுபடியாகாது, அதை ஆதரிக்காமல் இருக்கலாம்</translation>
     </message>
     <message>
         <source>Amazfish</source>
-        <translation type="unfinished"></translation>
+        <translation>அமாச்ஃபிச்</translation>
     </message>
     <message>
         <source>Connected</source>
-        <translation type="unfinished"></translation>
+        <translation>இணைக்கப்பட்டுள்ளது</translation>
     </message>
     <message>
         <source>Phone and watch are connected</source>
-        <translation type="unfinished"></translation>
+        <translation>தொலைபேசி மற்றும் வாட்ச் இணைக்கப்பட்டுள்ளன</translation>
     </message>
     <message>
         <source>Low Battery</source>
-        <translation type="unfinished"></translation>
+        <translation>குறைந்த பேட்டரி</translation>
     </message>
     <message>
         <source>Battery level now </source>
-        <translation type="unfinished"></translation>
+        <translation>இப்போது பேட்டரி நிலை </translation>
     </message>
 </context>
 <context>
     <name>DfuService</name>
     <message>
         <source>An operation is currently running, please try later</source>
-        <translation type="unfinished"></translation>
+        <translation>ஒரு செயல்பாடு தற்போது இயங்குகிறது, தயவுசெய்து பின்னர் முயற்சிக்கவும்</translation>
     </message>
     <message>
         <source>No file selected</source>
-        <translation type="unfinished"></translation>
+        <translation>எந்த கோப்பும் தேர்ந்தெடுக்கப்படவில்லை</translation>
     </message>
 </context>
 <context>
     <name>HuamiDevice</name>
     <message>
         <source>Navigation Started</source>
-        <translation type="unfinished"></translation>
+        <translation>வழிசெலுத்தல் தொடங்கியது</translation>
     </message>
     <message>
         <source>Navigation Stopped</source>
-        <translation type="unfinished"></translation>
+        <translation>வழிசெலுத்தல் நிறுத்தப்பட்டது</translation>
     </message>
     <message>
         <source>Progress</source>
-        <translation type="unfinished"></translation>
+        <translation>முன்னேற்றம்</translation>
     </message>
 </context>
 <context>
     <name>MiBandService</name>
     <message>
         <source>Firmware is too old to set display items, V0.1.1.14 is required</source>
-        <translation type="unfinished"></translation>
+        <translation>காட்சி உருப்படிகளை அமைக்க ஃபார்ம்வேர் மிகவும் பழையது, v0.1.1.14 தேவை</translation>
     </message>
     <message>
         <source>An operation is currently running, please try later</source>
-        <translation type="unfinished"></translation>
+        <translation>ஒரு செயல்பாடு தற்போது இயங்குகிறது, தயவுசெய்து பின்னர் முயற்சிக்கவும்</translation>
     </message>
 </context>
 <context>
     <name>QObject</name>
     <message>
         <source>About to transfer data from </source>
-        <translation type="unfinished"></translation>
+        <translation>தரவை மாற்றப் போவது </translation>
     </message>
     <message>
         <source>Finished transferring activity data</source>
-        <translation type="unfinished"></translation>
+        <translation>செயல்பாட்டு தரவை மாற்ற முடிந்தது</translation>
     </message>
     <message>
         <source>No data to transfer</source>
-        <translation type="unfinished"></translation>
+        <translation>மாற்ற தரவு இல்லை</translation>
     </message>
     <message>
         <source>Update operation failed</source>
-        <translation type="unfinished"></translation>
+        <translation>புதுப்பிப்பு செயல்பாடு தோல்வியடைந்தது</translation>
     </message>
     <message>
         <source>Update operation failed, unexpected metadata</source>
-        <translation type="unfinished"></translation>
+        <translation>புதுப்பிப்பு செயல்பாடு தோல்வியுற்றது, எதிர்பாராத மேனிலை தரவு</translation>
     </message>
     <message>
         <source>Update operation complete</source>
-        <translation type="unfinished"></translation>
+        <translation>புதுப்பிப்பு செயல்பாடு முடிந்தது</translation>
     </message>
     <message>
         <source>Update operation failed, filetype not supported</source>
-        <translation type="unfinished"></translation>
+        <translation>புதுப்பிப்பு செயல்பாடு தோல்வியடைந்தது, கோப்பு வகை ஆதரிக்கப்படவில்லை</translation>
     </message>
     <message>
         <source>File does not seem to be supported</source>
-        <translation type="unfinished"></translation>
+        <translation>கோப்பு ஆதரிக்கப்படுவதாகத் தெரியவில்லை</translation>
     </message>
 </context>
 </TS>
