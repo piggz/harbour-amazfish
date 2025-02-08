946426579e407eb070acd01be37b8670bf0bb045
Jozef Mlich <jmlich83@gmail.com>
2025-02-08 10:16:33 +0100

    update-qrc.sh should be sorted to return predictable output (to avoid random sort of items)

--- ui/update-qrc.sh
+++ ui/update-qrc.sh
@@ -10,12 +10,12 @@ for platform in ${platforms[@]}; do
         echo "<RCC>"
         echo "    <qresource prefix=\"/\">"
         replace=platform.${platform}
-        for i in $(find ./qml/components/ -path '*platform.'"$platform"'*' -name '*.qml'); do
+        for i in $(find ./qml/components/ -path '*platform.'"$platform"'*' -name '*.qml'|sort); do
             x=${i//$replace/platform}; 
             echo "        <file alias=\"$x\">$i</file>";
         done
 
-        for i in $(find ./qml/components/ -maxdepth 1 -name '*.qml'); do
+        for i in $(find ./qml/components/ -maxdepth 1 -name '*.qml'|sort); do
             echo "        <file>$i</file>";
         done
         echo "    </qresource>"
@@ -28,7 +28,7 @@ done
     (
         echo "<RCC>"
         echo "    <qresource prefix=\"/\">"
-        for i in $(find qml/custom-icons/ -type f -name '*.png'); do
+        for i in $(find qml/custom-icons/ -type f -name '*.png'|sort); do
             echo "        <file>$i</file>";
         done
         echo "    </qresource>"
