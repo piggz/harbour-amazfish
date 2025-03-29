25f1454d8715fca924d476460092c6f97df79614
Danial Behzadi <dani.behzi@ubuntu.com>
2025-03-29 13:55:39 +0100

    Translated using Weblate (Persian)
    Currently translated at 100.0% (29 of 29 strings)
    
    Translation: Amazfish/Daemon
    Translate-URL: https://hosted.weblate.org/projects/harbour-amazfish/daemon/fa/

--- daemon/translations/harbour-amazfishd-fa.ts
+++ daemon/translations/harbour-amazfishd-fa.ts
@@ -5,143 +5,143 @@
     <name>AbstractDevice</name>
     <message>
         <source>Device does not support settings</source>
-        <translation type="unfinished"></translation>
+        <translation>افزاره از تنظیمات پشتیبانی نمی‌کند</translation>
     </message>
 </context>
 <context>
     <name>AsteroidOSDevice</name>
     <message>
         <source>Stored %1...</source>
-        <translation type="unfinished"></translation>
+        <translation>ذخیره کردن %1…</translation>
     </message>
 </context>
 <context>
     <name>BipDevice</name>
     <message>
         <source>Navigation Started</source>
-        <translation type="unfinished"></translation>
+        <translation>ناوبری آغاز شد</translation>
     </message>
     <message>
         <source>Navigation Stopped</source>
-        <translation type="unfinished"></translation>
+        <translation>ناوبری پایان یافت</translation>
     </message>
     <message>
         <source>Progress</source>
-        <translation type="unfinished"></translation>
+        <translation>پیشرفت</translation>
     </message>
 </context>
 <context>
     <name>BipFirmwareService</name>
     <message>
         <source>An operation is currently running, please try later</source>
-        <translation type="unfinished"></translation>
+        <translation>عملیاتی در حال اجراست. تلاش دوباره در بعد</translation>
     </message>
     <message>
         <source>Sending %1...</source>
-        <translation type="unfinished"></translation>
+        <translation>فرستادن %1…</translation>
     </message>
     <message>
         <source>No file selected</source>
-        <translation type="unfinished"></translation>
+        <translation>هیچ پرونده‌ای گزیده نشده</translation>
     </message>
 </context>
 <context>
     <name>DeviceInterface</name>
     <message>
         <source>Device is not valid, it may not be supported</source>
-        <translation type="unfinished"></translation>
+        <translation>افزاره معتبر نیست. ممکمن است پشتیبانی نشود</translation>
     </message>
     <message>
         <source>Amazfish</source>
-        <translation type="unfinished"></translation>
+        <translation>امیزفیش</translation>
     </message>
     <message>
         <source>Connected</source>
-        <translation type="unfinished"></translation>
+        <translation>وصل&#xa0;شده</translation>
     </message>
     <message>
         <source>Phone and watch are connected</source>
-        <translation type="unfinished"></translation>
+        <translation>تلفن و ساعت وصل شده‌اند</translation>
     </message>
     <message>
         <source>Low Battery</source>
-        <translation type="unfinished"></translation>
+        <translation>باتری کم است</translation>
     </message>
     <message>
         <source>Battery level now </source>
-        <translation type="unfinished"></translation>
+        <translation>سطح کنونی باتری </translation>
     </message>
 </context>
 <context>
     <name>DfuService</name>
     <message>
         <source>An operation is currently running, please try later</source>
-        <translation type="unfinished"></translation>
+        <translation>عملیاتی در حال اجراست. تلاش دوباره در بعد</translation>
     </message>
     <message>
         <source>No file selected</source>
-        <translation type="unfinished"></translation>
+        <translation>هیچ پرونده‌ای گزیده نشده</translation>
     </message>
 </context>
 <context>
     <name>HuamiDevice</name>
     <message>
         <source>Navigation Started</source>
-        <translation type="unfinished"></translation>
+        <translation>ناوبری آغاز شد</translation>
     </message>
     <message>
         <source>Navigation Stopped</source>
-        <translation type="unfinished"></translation>
+        <translation>ناوبری پایان یافت</translation>
     </message>
     <message>
         <source>Progress</source>
-        <translation type="unfinished"></translation>
+        <translation>پیشرفت</translation>
     </message>
 </context>
 <context>
     <name>MiBandService</name>
     <message>
         <source>Firmware is too old to set display items, V0.1.1.14 is required</source>
-        <translation type="unfinished"></translation>
+        <translation>ثابت‌افزار برای تنظیم مورد‌های نمایش بیش از حد قدیمی است. نیازمند V0.1.1.14</translation>
     </message>
     <message>
         <source>An operation is currently running, please try later</source>
-        <translation type="unfinished"></translation>
+        <translation>عملیاتی در حال اجراست. تلاش دوباره در بعد</translation>
     </message>
 </context>
 <context>
     <name>QObject</name>
     <message>
         <source>About to transfer data from </source>
-        <translation type="unfinished"></translation>
+        <translation>آمادهٔ انتقال داده‌ها از </translation>
     </message>
     <message>
         <source>Finished transferring activity data</source>
-        <translation type="unfinished"></translation>
+        <translation>انتقال داده‌های فعّالیت پایان یافت</translation>
     </message>
     <message>
         <source>No data to transfer</source>
-        <translation type="unfinished"></translation>
+        <translation>هیچ داده‌ای برای انتقال وجود ندارد</translation>
     </message>
     <message>
         <source>Update operation failed</source>
-        <translation type="unfinished"></translation>
+        <translation>عملیات به‌روز&#xa0;رسانی شکست خورد</translation>
     </message>
     <message>
         <source>Update operation failed, unexpected metadata</source>
-        <translation type="unfinished"></translation>
+        <translation>عملیات به‌روز&#xa0;رسانی شکست خورد. فرادادهٔ غیرمنتظره</translation>
     </message>
     <message>
         <source>Update operation complete</source>
-        <translation type="unfinished"></translation>
+        <translation>عملیات به‌روز&#xa0;رسانی کامل شد</translation>
     </message>
     <message>
         <source>Update operation failed, filetype not supported</source>
-        <translation type="unfinished"></translation>
+        <translation>عملیات به‌روز&#xa0;رسانی شکست خورد. گونهٔ پرونده پشتیبانی نمی‌شود</translation>
     </message>
     <message>
         <source>File does not seem to be supported</source>
-        <translation type="unfinished"></translation>
+        <translation>پرونده پشتیبانی&#xa0;شده به نظر نمی‌رسد</translation>
     </message>
 </context>
 </TS>
