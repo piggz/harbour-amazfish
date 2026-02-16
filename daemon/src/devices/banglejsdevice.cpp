d7ec56559a99cf6782e0dc528c8444ab6c01fef0
Jozef Mlich <jmlich83@gmail.com>
2026-02-16 19:28:57 +0100

    Remove unused code

--- daemon/src/devices/banglejsdevice.cpp
+++ daemon/src/devices/banglejsdevice.cpp
@@ -532,6 +532,7 @@ void BangleJSDevice::handleRxJson(const QJsonObject &json)
         for (const QJsonValue &value : trksList) {
             QString listItem = value.toString();
             fetchActivityRec(listItem);
+            QString lastSyncId = AmazfishConfig::instance()->value("device/lastsportsyncid").toString();
             break;
         }
     } else if (t == "actTrk") {
@@ -794,101 +795,5 @@ QString BangleJSDevice::activityRecordsToText(const QString& logId)
 }
 
 
-QString BangleJSDevice::activityRecordsToTCX(const QString& logId)
-{
-    QString str;
-/*
-    QTextStream out(&str);
-    out.setRealNumberPrecision(10);
-
-    out << "<?xml version=\"1.0\" standalone=\"yes\"?>" << endl;
-    out << "<TrainingCenterDatabase" << endl;
-    out << "xmlns=\"http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2\"" << endl;
-    out << "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"" << endl;
-    out << "xsi:schemaLocation=\"http://www.garmin.com/xmlschemas/ActivityExtension/v2" << endl;
-    out << "http://www.garmin.com/xmlschemas/ActivityExtensionv2.xsd" << endl;
-    out << "http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2" << endl;
-    out << "http://www.garmin.com/xmlschemas/TrainingCenterDatabasev2.xsd\">" << endl;
-
-    out << "<Activities>" << endl;
-    out << "<Activity Sport=\"" + ActivityKind::toString(m_summary.activityKind()) + "\">" << endl;
-    out << "<Id>" << m_summary.name() << "</Id>" << endl;
-    out << "<Lap StartTime=\"" << m_summary.startTime().toTimeSpec(Qt::OffsetFromUTC).toString(Qt::ISODate) << "\">" << endl;
-
-    //TotalTimeSeconds
-    ActivitySummary::meta m = m_summary.metaData("activeSeconds");
-    if (m.key == "activeSeconds") {
-        out << "<TotalTimeSeconds>" << m.value << "</TotalTimeSeconds>" << endl;
-    }
-
-    //DistanceMeters
-    m = m_summary.metaData("distanceMeters");
-    if (m.key == "distanceMeters") {
-        out << "<DistanceMeters>" << m.value << "</DistanceMeters>" << endl;
-    }
-
-    //Calories
-    m = m_summary.metaData("caloriesBurnt");
-    if (m.key == "caloriesBurnt") {
-        out << "<Calories>" << m.value << "</Calories>" << endl;
-    }
-
-    //AverageHeartRateBpm
-    m = m_summary.metaData("averageHR");
-    if (m.key == "averageHR") {
-        out << "<AverageHeartRateBpm><Value>" << m.value << "</Value></AverageHeartRateBpm>" << endl;
-    }
-
-    //MaximumHeartRateBpm
-    m = m_summary.metaData("maxHR");
-    if (m.key == "maxHR") {
-        out << "<MaximumHeartRateBpm><Value>" << m.value << "</Value></MaximumHeartRateBpm>" << endl;
-    }
-
-    out << "<Track>" << endl;
-
-    foreach(ActivityCoordinate pos, m_activityTrack) {
-        out << "<Trackpoint>" << endl;
-        QDateTime dt = pos.timeStamp();
-        //dt.setTimeZone(QTimeZone::systemTimeZone());
-        //dt.setTimeSpec(Qt::OffsetFromUTC);
-        out << "<Time>" << dt.toTimeSpec(Qt::OffsetFromUTC).toString(Qt::ISODate) << "</Time>" << endl;
-
-        if (pos.coordinate().isValid()) {
-            out << "  <Position>" << endl;
-            out << "    <LatitudeDegrees>" << pos.coordinate().latitude() << "</LatitudeDegrees>" << endl;
-            out << "    <LongitudeDegrees>" << pos.coordinate().longitude() << "</LongitudeDegrees>" << endl;
-            out << "  </Position>" << endl;
-            out << "  <AltitudeMeters>" << pos.coordinate().altitude() << "</AltitudeMeters>" << endl;
-        }
-        out << "<HeartRateBpm xsi:type=\"HeartRateInBeatsPerMinute_t\"><Value>" << pos.heartRate() << "</Value></HeartRateBpm>" << endl;
-        out << "</Trackpoint>" << endl;
-    }
-
-    out << "</Track>" << endl;
-
-    //Steps
-    m = m_summary.metaData("steps");
-    if (m.key == "steps") {
-        out << "<Extensions>" << endl;
-        out << "  <LX xmlns=\"http://www.garmin.com/xmlschemas/ActivityExtension/v2\">" << endl;
-        out << "    <Steps>" << m.value << "</Steps>" << endl;
-        out << "  </LX>" << endl;
-        out << "</Extensions>" << endl;
-    }
-
-    out << "</Lap>" << endl;
-
-    //Creator
-    out << "<Creator xsi:type=\"Device_t\"><Name>" << AmazfishConfig::instance()->pairedName() << "</Name><UnitId>0000000000</UnitId><ProductId>0000</ProductId><Version><VersionMajor>1</VersionMajor><VersionMinor>0</VersionMinor><BuildMajor>1</BuildMajor><BuildMinor>0</BuildMinor></Version></Creator>" << endl;
-
-    out << "</Activity>" << endl;
-    out << "</Activities>" << endl;
-
-    //Author
-    out << "<Author xsi:type=\"Application_t\"><Name>Amazfish</Name><Build><Version><VersionMajor>1</VersionMajor><VersionMinor>0</VersionMinor><BuildMajor>1</BuildMajor><BuildMinor>0</BuildMinor></Version></Build><LangID>en</LangID><PartNumber>000-00000-00</PartNumber></Author>" << endl;
-
-    out << "</TrainingCenterDatabase>" << endl;
-*/
-    return str;
-}
+//    QString AbstractActivityDetailParser::toTCX()
+// QString BangleJSDevice::activityRecordsToTCX(const QString& logId)
