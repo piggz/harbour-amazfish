13266ffe6cda9cccbcaa133d3ad95d2719717f0f
Frank Paul Silye <frankps@gmail.com>
2026-02-22 21:35:28 +0100

    Translated using Weblate (Norwegian Bokmål)
    Currently translated at 100.0% (49 of 49 strings)
    
    Translation: Amazfish/Daemon
    Translate-URL: https://hosted.weblate.org/projects/harbour-amazfish/daemon/nb_NO/

--- daemon/translations/harbour-amazfishd-nb_NO.ts
+++ daemon/translations/harbour-amazfishd-nb_NO.ts
@@ -170,7 +170,7 @@
     <message>
         <location filename="../src/devices/huami/huamifetcher.cpp" line="99"/>
         <source>All operations cancelled</source>
-        <translation type="unfinished"></translation>
+        <translation>Alle operasjoner avbrutt</translation>
     </message>
     <message>
         <location filename="../src/devices/huami/huamifetcher.cpp" line="135"/>
