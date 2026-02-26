74e2a734adcbfaebfd7cebd73feccbcf3e2a0e93
Frank Paul Silye <frankps@gmail.com>
2026-02-26 10:21:14 +0100

    Translated using Weblate (Norwegian Bokmål)
    Currently translated at 100.0% (50 of 50 strings)
    
    Translation: Amazfish/Daemon
    Translate-URL: https://hosted.weblate.org/projects/harbour-amazfish/daemon/nb_NO/

--- daemon/translations/harbour-amazfishd-nb_NO.ts
+++ daemon/translations/harbour-amazfishd-nb_NO.ts
@@ -293,7 +293,7 @@
     <message>
         <location filename="../src/services/uartservice.cpp" line="58"/>
         <source>Android Integration plugin not installed on Bangle.js</source>
-        <translation type="unfinished"></translation>
+        <translation>Android Integration-tillegg er ikke installert på Bangle.js</translation>
     </message>
 </context>
 <context>
