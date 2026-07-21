7a25469681cdcd285fce6ce0a54783991f1460c5
Adam Pigg <adam@piggz.co.uk>
2026-07-21 10:49:33 +0100

    Sleep data now processed.
    Might not be completely correct, some values look a little large, but
    not hugely off.

--- ui/src/zepposdatasource.cpp
+++ ui/src/zepposdatasource.cpp
@@ -38,52 +38,59 @@ QList<DataSource::SleepSession> ZeppOSDataSource::calculateSleep(const QDate &da
                     qint64 d = curs->value(0).toLongLong();
                     QByteArray raw = QByteArray::fromBase64(curs->value(1).toString().toLocal8Bit());
 
-                    int32_t timestampSeconds = TypeConversion::toUint32(raw, offset);
-                    QDateTime timestamp;
-                    timestamp.setMSecsSinceEpoch((qint64)timestampSeconds * 1000);
+                    qDebug() << "Raw sleep data length:" << raw.length();
 
-                    int32_t timestampSecondsMidnight = TypeConversion::toUint32(raw, offset);
-                    QDateTime timestampMidnight;
-                    timestampMidnight.setMSecsSinceEpoch((qint64)timestampSecondsMidnight * 1000);
+                    if (raw.length() == 594) {
+                        int32_t timestampSeconds = TypeConversion::toUint32(raw, offset);
+                        QDateTime timestamp;
+                        timestamp.setMSecsSinceEpoch((qint64)timestampSeconds * 1000);
 
-                    //skip 2 bytes
-                    offset++;
-                    offset++;
+                        int32_t timestampSecondsMidnight = TypeConversion::toUint32(raw, offset);
+                        QDateTime timestampMidnight;
+                        timestampMidnight.setMSecsSinceEpoch((qint64)timestampSecondsMidnight * 1000);
 
-                    uint16_t sleepStart = TypeConversion::toUint16(raw, offset);
-                    uint16_t sleepEnd = TypeConversion::toUint16(raw, offset);
+                        //skip 2 bytes
+                        offset++;
+                        offset++;
 
-                    offset = 0x15;
-                    uint8_t avgHr = TypeConversion::toUint8(raw, offset);
-                    uint8_t score = TypeConversion::toUint8(raw, offset);
+                        uint16_t sleepStart = TypeConversion::toUint16(raw, offset);
+                        uint16_t sleepEnd = TypeConversion::toUint16(raw, offset);
 
-                    offset = 0x54;
+                        offset = 0x15;
+                        uint8_t avgHr = TypeConversion::toUint8(raw, offset);
+                        uint8_t score = TypeConversion::toUint8(raw, offset);
 
-                    int numStages = TypeConversion::toInt32(raw, offset);
-                    for (int i = 0; i < numStages; i++) {
-                        offset = 0x56 + 5 * i;
-                        uint16_t stageStart = TypeConversion::toUint16(raw, offset);
-                        uint16_t stageEnd = TypeConversion::toUint16(raw, offset);
-                        uint16_t stageType = TypeConversion::toUint8(raw, offset);
+                        offset = 0x54;
 
-                        qDebug() << "Sleep stage:" << stageStart << stageEnd << stageType;
+                        int numStages = TypeConversion::toUint8(raw, offset);
+
+                        qDebug() << "Num sleep stages:" << numStages;
+                        for (int i = 0; i < numStages; i++) {
+                            offset = 0x56 + 5 * i;
+                            uint16_t stageStart = TypeConversion::toUint16(raw, offset);
+                            uint16_t stageEnd = TypeConversion::toUint16(raw, offset);
+                            uint16_t stageType = TypeConversion::toUint8(raw, offset);
+
+                            qDebug() << "Sleep stage:" << stageStart << stageEnd << stageType;
+                        }
+
+                        offset = 0x024a;
+                        uint16_t totalRemMinutes = TypeConversion::toUint16(raw, offset);
+                        uint16_t totalLightMinutes = TypeConversion::toUint16(raw, offset);
+                        uint16_t totalDeepMinutes = TypeConversion::toUint16(raw, offset);
+                        uint16_t totalWakeMinutes = TypeConversion::toUint16(raw, offset);
+
+                        SleepSession ss;
+                        ss.sleepStart = timestamp;
+                        ss.sleepEnd = timestamp;
+                        ss.lightSleepDuration = totalLightMinutes * 60;
+                        ss.deepSleepDuration = totalDeepMinutes * 60;
+                        ss.remSleepDuration = totalRemMinutes * 60;
+                        ss.awakeDuration = totalWakeMinutes * 60;
+                        sessions << ss;
+
+                        qDebug() << "Sleep Session:" << ss.sleepStart << ss.lightSleepDuration << ss.deepSleepDuration << ss.remSleepDuration << ss.awakeDuration;
                     }
-
-                    offset = 0x024a;
-                    uint16_t totalRemMinutes = TypeConversion::toUint16(raw, offset);
-                    uint16_t totalLightMinutes = TypeConversion::toUint16(raw, offset);
-                    uint16_t totalDeepMinutes = TypeConversion::toUint16(raw, offset);
-                    uint16_t totalWakeMinutes = TypeConversion::toUint16(raw, offset);
-
-                    SleepSession ss;
-                    ss.sleepStart = timestamp;
-                    ss.sleepEnd = timestamp;
-                    ss.lightSleepDuration = totalLightMinutes;
-                    ss.deepSleepDuration = totalDeepMinutes;
-                    ss.remSleepDuration = totalRemMinutes;
-                    ss.awakeDuration = totalWakeMinutes;
-                    sessions << ss;
-
                     curs->moveNext();
                 }
             }
