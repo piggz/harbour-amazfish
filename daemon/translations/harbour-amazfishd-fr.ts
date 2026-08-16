d2e010057d4c8e567a34519a5b1e12c4d554506c
idriss nicray <nicrayt@gmail.com>
2026-08-16 17:19:05 +0200

    Translated using Weblate (French)
    Currently translated at 100.0% (53 of 53 strings)
    
    Translation: Amazfish/Daemon
    Translate-URL: https://hosted.weblate.org/projects/harbour-amazfish/daemon/fr/

--- daemon/translations/harbour-amazfishd-fr.ts
+++ daemon/translations/harbour-amazfishd-fr.ts
@@ -79,7 +79,7 @@
     <message>
         <location filename="../src/achievements.cpp" line="46"/>
         <source>No data</source>
-        <translation type="unfinished"></translation>
+        <translation>Pas de donnée</translation>
     </message>
 </context>
 <context>
@@ -95,12 +95,12 @@
     <message>
         <location filename="../src/devices/banglejsdevice.cpp" line="349"/>
         <source>Downloading activity data</source>
-        <translation type="unfinished"></translation>
+        <translation>Téléchargement des données d&apos;activités</translation>
     </message>
     <message>
         <location filename="../src/devices/banglejsdevice.cpp" line="370"/>
         <source>Downloading sports data</source>
-        <translation type="unfinished"></translation>
+        <translation>Téléchargement des données de sports</translation>
     </message>
 </context>
 <context>
